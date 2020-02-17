3a6b9433742a79a1e8d6bd685977b1e0aac22eae
Seb Garwood <sebastiangarwood@outlook.com>
2020-02-17 17:34:25 +0000

    Updating local run configuration, dockerfiles and config for database

--- api/src/main/java/uk/ac/leeds/comp2913/api/Controller/BookingController.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Controller/BookingController.java
@@ -1,15 +1,15 @@
 package uk.ac.leeds.comp2913.api.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.leeds.comp2913.api.Domain.Model.Booking;
 
 @RestController
+@RequestMapping("/booking")
 public class BookingController {
-
-
-  @GetMapping("/booking")
+  @GetMapping("")
   public Booking booking(@RequestParam(value = "name", defaultValue = "World") String name) {
     return new Booking();
   }
