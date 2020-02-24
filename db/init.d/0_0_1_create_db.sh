480f2927c7ea763da68f8b8bd2d266b8584c9241
Seb Garwood <sebastiangarwood@outlook.com>
2020-02-24 20:45:26 +0000

    Fix typo

--- db/init.d/0_0_1_create_db.sh
+++ db/init.d/0_0_1_create_db.sh
@@ -5,5 +5,5 @@ set -e
 #    CREATE DATABASE sports_centre_managemnet;
 #    GRANT ALL PRIVILEGES ON DATABASE sports_centre_managemnet TO sports_centre_managemnet;
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE SCHEMA sports_management_centre AUTHORIZATION comp2913;
+    CREATE SCHEMA sports_centre_management AUTHORIZATION comp2913;
 EOSQL
