61c8e8a1e989d1d7ec42da91d6285a5528ee55e9
tzerom <sc18tjo@leeds.ac.uk>
2020-03-02 18:29:37 +0000

    Implemented test classes for account, booking, customer, and membership classes

--- api/src/test/java/uk/ac/leeds/comp2913/api/Domain/Model/AccountTest.java
+++ api/src/test/java/uk/ac/leeds/comp2913/api/Domain/Model/AccountTest.java
@@ -2,14 +2,24 @@ package uk.ac.leeds.comp2913.api.Domain.Model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.util.Date;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
+    Account account;
+    Random random;
+
     @BeforeEach
     void setUp() {
+        account = new Account();
+        random = new Random();
     }
 
     @AfterEach
@@ -18,29 +28,51 @@ class AccountTest {
 
     @Test
     void getId() {
+        long testId = account.getId();
+        assertTrue(testId > 0);
     }
 
     @Test
     void getCreatedAt() {
+        Date testDateSet = new Date(random.nextLong());
+        account.setCreatedAt(testDateSet);
+        Date testDateGet = account.getCreatedAt();
+        assertEquals(testDateSet, testDateGet);
     }
 
     @Test
     void setCreatedAt() {
+        Date testDateSet = new Date(random.nextLong());
+        account.setCreatedAt(testDateSet);
+        Date testDateGet = account.getCreatedAt();
+        assertEquals(testDateSet, testDateGet);
     }
 
     @Test
     void getUpdatedAt() {
+        Date testDateSet = new Date(random.nextLong());
+        account.setUpdatedAt(testDateSet);
+        Date testDateGet = account.getUpdatedAt();
+        assertEquals(testDateSet, testDateGet);
     }
 
     @Test
     void setUpdatedAt() {
+        Date testDateSet = new Date(random.nextLong());
+        account.setUpdatedAt(testDateSet);
+        Date testDateGet = account.getUpdatedAt();
+        assertEquals(testDateSet, testDateGet);
     }
 
     @Test
+    @Disabled
     void buyMembership() {
+        // TODO Not implemented
     }
 
     @Test
+    @Disabled
     void cancelMembership() {
+        // TODO Not implemented
     }
 }
