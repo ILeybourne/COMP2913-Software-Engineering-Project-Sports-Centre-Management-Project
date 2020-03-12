6cabefc5c9aae1d5ea3e5dda4a11c4190f130fcc
Seb Garwood <sebastiangarwood@outlook.com>
2020-03-12 10:00:54 +0000

    Fixing CORS issues :((((

--- api/src/main/java/uk/ac/leeds/comp2913/api/Security/OAuth2ResourceServerSecurityConfiguration.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Security/OAuth2ResourceServerSecurityConfiguration.java
@@ -2,12 +2,23 @@ package uk.ac.leeds.comp2913.api.Security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -19,14 +30,13 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
   protected void configure(HttpSecurity http) throws Exception {
     // @formatter:off
     http
-        .csrf().disable()
-        .cors().and()
-        .authorizeRequests(authorizeRequests ->
-        authorizeRequests
-//          TODO: Public endpoints for timetable
-//          .antMatchers(HttpMethod.POST, "/test/permission/**").hasAuthority("SCOPE_message:write")
-          .anyRequest().authenticated()
-      )
+      .cors().and()
+      .csrf().disable()
+      .authorizeRequests()
+//        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        .antMatchers("/public/**").permitAll()
+        .anyRequest().authenticated()
+      .and()
       .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     // @formatter:on
   }
@@ -35,5 +45,22 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
   JwtDecoder jwtDecoder() {
     return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
   }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+    // setAllowCredentials(true) is important, otherwise:
+    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+    configuration.setAllowCredentials(true);
+    // setAllowedHeaders is important! Without it, OPTIONS preflight request
+    // will fail with 403 Invalid CORS request
+    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
 }
 
