e9e14b77ac1a9d9ddb46b69cc84d3746afa7278b
Seb Garwood <sebastiangarwood@outlook.com>
2020-03-12 13:09:28 +0000

    Fixing migrations

--- api/src/main/resources/db/migration/V1_20__CREATE_RECEIPT.sql
+++
@@ -1,8 +0,0 @@
-create table sports_centre_management.receipt
-(
-    id                  int8 not null,
-    created_at          timestamp,
-    product_description text,
-    cost_gbp_pence      int,
-    primary key (id)
-);
