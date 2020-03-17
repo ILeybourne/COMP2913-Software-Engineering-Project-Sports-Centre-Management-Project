d06b2c1578578fb79964412c7c1b5d7d0f368d38
Izzywu <izzywu@hotmail.com>
2020-03-17 20:53:16 +0000

    booking, guest, and billing validation added

--- web-frontend/customer-app/src/components/PaymentInformation.vue
+++ web-frontend/customer-app/src/components/PaymentInformation.vue
@@ -4,11 +4,23 @@
       class="payment-container"
       v-bind:style="{ width: this.componentWidth + '%' }"
     >
-      <form>
+      <form @submit="[setPaymentInfoToParent]">
         <!--        TODO Fill from customer details-->
         <div class="form-row">
           <label for="name">Name On Card:</label>
-          <input type="text" id="name" name="name" v-model="name" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            v-model="name"
+            v-bind:class="{
+              'is-valid': nameValid,
+              'is-invalid': !nameValid
+            }"
+            class="form-control"
+            @keyup="validateName"
+            @change="validateName"
+          />
         </div>
         <div class="form-row">
           <label for="cardType">Card Type:</label>
@@ -17,6 +29,13 @@
             id="cardType"
             name="cardType"
             v-model="cardType"
+            v-bind:class="{
+              'is-valid': cardTypeValid,
+              'is-invalid': !cardTypeValid
+            }"
+            class="form-control"
+            @keyup="validateCardType"
+            @change="validateCardType"
           />
         </div>
         <div class="form-row">
@@ -26,6 +45,13 @@
             id="cardNumber"
             name="cardNumber"
             v-model="cardNumber"
+            v-bind:class="{
+              'is-valid': cardNumberValid,
+              'is-invalid': !cardNumberValid
+            }"
+            class="form-control"
+            @keyup="validateCardNumber"
+            @change="validateCardNumber"
           />
         </div>
         <div class="form-row">
@@ -39,14 +65,20 @@
             id="secureCode"
             name="secureCode"
             v-model="secureCode"
+            v-bind:class="{
+              'is-valid': secureCodeValid,
+              'is-invalid': !secureCodeValid
+            }"
+            class="form-control"
+            @keyup="validateSecureCode"
+            @change="validateSecureCode"
           />
         </div>
         <div class="button-container">
           <button
-            type="button"
+            type="submit"
             class="btn btn-outline-secondary"
             name="details"
-            @click="setPaymentInfoToParent()"
           >
             Submit
           </button>
@@ -110,7 +142,12 @@ export default {
       cardNumber: "",
       date: "",
       secureCode: "",
-      componentWidth: 90
+      componentWidth: 90,
+      nameValid: "",
+      cardTypeValid: "",
+      cardNumberValid: "",
+      dateValid: "",
+      secureCodeValid: ""
     };
   },
   computed: {},
@@ -119,6 +156,57 @@ export default {
       //TODO Validate before showing 2nd form
       this.componentWidth = 60;
       this.$emit("setPaymentInfoToParent", this.$data);
+    },
+    validateName() {
+      this.nameValid = this.$data.name !== "";
+    },
+    validateCardType() {
+      this.cardTypeValid = this.$data.cardType !== "";
+    },
+    validateCardNumber() {
+      this.cardNumberValid = this.$data.cardNumber !== "";
+    },
+    validateDate() {
+      this.dateValid = this.$data.date !== "";
+    },
+    validateSecureCode() {
+      this.secureCodeValid = this.$data.secureCode !== "";
+    },
+
+    callValidation() {
+      this.validateName();
+      this.validateCardType();
+      this.validateCardNumber();
+      this.validateDate();
+      this.validateSecureCode();
+    },
+
+    submitCustomerDetails(e) {
+      //TODO Validate before showing 2nd form
+      console.log("e");
+      console.log(e);
+      e.preventDefault();
+      //TODO send array of data to parent
+      console.log("this.$data");
+      console.log(this.$data);
+      if (
+        this.$data.name !== "" &&
+        this.$data.cardType !== "" &&
+        this.$data.cardNumber !== "" &&
+        this.$data.date !== "" &&
+        this.$data.secureCode !== ""
+      ) {
+        this.$emit("submitCustomerDetails", this.$data);
+        this.componentWidth = 60;
+        this.nameValid = true;
+        this.cardTypeValid = true;
+        this.cardNumberValid = true;
+        this.dateValid = true;
+        this.secureCode = true;
+      } else {
+        console.log("wont emit");
+        this.callValidation();
+      }
     }
   }
 };
