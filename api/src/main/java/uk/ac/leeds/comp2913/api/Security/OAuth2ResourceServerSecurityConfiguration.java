8d5ccee775216ad56115210cc8cbdbef7ac75eb1
Seb Garwood <sebastiangarwood@outlook.com>
2020-03-30 16:45:21 +0100

    Fixing Security Issues with Tokens

--- api/src/main/java/uk/ac/leeds/comp2913/api/Security/OAuth2ResourceServerSecurityConfiguration.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Security/OAuth2ResourceServerSecurityConfiguration.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoderJwkSupport;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.JwtBearerTokenAuthenticationConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -32,14 +37,15 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
       .cors().and()
       .csrf().disable()
       .authorizeRequests()
-//        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-        .antMatchers("/public/**").permitAll()
+        .mvcMatchers("/public/**").permitAll()
         .anyRequest().authenticated()
       .and()
       .oauth2ResourceServer()
             .authenticationEntryPoint(authEntryPoint())
-            .jwt()
-                .jwtAuthenticationConverter(jwtAuthenticationDecoder());
+            .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                    .decoder(jwtDecoder())
+            );
     // @formatter:on
     }
 
@@ -47,19 +53,26 @@ public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfig
         return new MoreInformativeAuthenticationEntryPoint();
     }
 
-    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationDecoder() {
-        return new GrantedAuthoritiesExtractor();
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new GrantedAuthoritiesExtractor());
+        return jwtAuthenticationConverter;
     }
 
     @Bean
     JwtDecoder jwtDecoder() {
-        NimbusJwtDecoderJwkSupport decoder = (NimbusJwtDecoderJwkSupport)
-                JwtDecoders.fromOidcIssuerLocation(this.issuerUri);
-
         OAuth2TokenValidator<Jwt> springDefaults = JwtValidators.createDefaultWithIssuer(this.issuerUri);
 
-        decoder.setJwtValidator(springDefaults);
-        return decoder;
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(this.issuerUri);
+        jwtDecoder.setJwtValidator(springDefaults);
+//        jwtDecoder.setClaimSetConverter();
+        return jwtDecoder;
+    }
+
+    AuthenticationManager jwt() {
+        JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(this.jwtDecoder());
+        authenticationProvider.setJwtAuthenticationConverter(new JwtBearerTokenAuthenticationConverter());
+        return authenticationProvider::authenticate;
     }
 
     @Bean
