f5b828ed3c593dd7bac6807ca2c4ee2dd9409337
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-01 15:23:26 +0100

    WIP: new vue

---
+++ web-frontend/customer-app/src/components/CentrePriceListing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="price-table">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Centre Price Listing",
+    props: { centre: Object },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .price-table {
+    width: 100%
+  }
+</style>
