c84fe3763bab3b37c9716bd75525324a13ae8f0a
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-02 03:03:55 +0100

    Adding TODOS, chores, docblocks, reformatting files, removing old methods

--- api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ReceiptController.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ReceiptController.java
@@ -4,21 +4,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import uk.ac.leeds.comp2913.api.DataAccessLayer.Repository.ReceiptRepository;
-import uk.ac.leeds.comp2913.api.Domain.Model.Customer;
 import uk.ac.leeds.comp2913.api.Domain.Model.Receipt;
 import uk.ac.leeds.comp2913.api.Domain.Service.CustomerService;
 import uk.ac.leeds.comp2913.api.Domain.Service.ReceiptService;
 import uk.ac.leeds.comp2913.api.Exception.ResourceNotFoundException;
-import uk.ac.leeds.comp2913.api.ViewModel.CreateReceiptDto;
 
-import javax.validation.Valid;
-
+/**
+ * TODO: @RESEARCH how we can move the regular booking and memberships into scheduled service
+ */
 @PreAuthorize("hasAuthority('SCOPE_customer')")
 @RestController
 @RequestMapping("/receipts")
@@ -26,41 +26,43 @@ public class ReceiptController {
 
     private final ReceiptRepository receiptRepository;
     private ReceiptService receiptService;
-    private CustomerService customerService;
 
 
     public ReceiptController(ReceiptRepository receiptRepository, ReceiptService receiptService, CustomerService customerService) {
         this.receiptRepository = receiptRepository;
         this.receiptService = receiptService;
-        this.customerService = customerService;
     }
 
+    /**
+     * Get all receipts generated in this application
+     *
+     * @param pageable
+     * @return
+     */
     @GetMapping("")
     public Page<Receipt> getReceipts(Pageable pageable) {
         return receiptRepository.findAll(pageable);
     }
 
-    @PreAuthorize("hasAuthority('SCOPE_create:receipt')")
-    @PostMapping("")
-    public Receipt createReceipt(@Valid @RequestBody CreateReceiptDto receipt, @AuthenticationPrincipal Jwt oidcUser) {
-        System.out.println(oidcUser.getClaims());
-        System.out.println(oidcUser.getSubject());
-        Customer c = this.customerService.getCustomerByEmailAddress(oidcUser.getSubject());
-        return this.receiptService.invoice(receipt.getTransactionId(), receipt.getSales(), c);
-    }
-
-    @PutMapping("/{receipt_id}")
-    public Receipt updateReceipt(@PathVariable Long receipt_id, @Valid @RequestBody Receipt receiptRequest) {
+    /**
+     * TODO: add endpoint that return file download of the receipt PDF
+     *
+     * @param receipt_id
+     * @return
+     */
+    @GetMapping("/{receipt_id}")
+    public Receipt getReceipt(@PathVariable Long receipt_id) {
         return receiptRepository.findById(receipt_id)
-                .map(receipt -> {
-                    receipt.setProductDescription(receiptRequest.getProductDescription());
-                    receipt.setTotal(receiptRequest.getTotal());
-                    receipt.setSales(receiptRequest.getSales());
-                    //todo add setPayments
-                    return receiptRepository.save(receipt);
-                }).orElseThrow(() -> new ResourceNotFoundException("Resource not found with id " + receipt_id));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Receipt not found with id " + receipt_id)
+                );
     }
 
+
+    /**
+     * @param receipt_id
+     * @return
+     */
     @DeleteMapping("/{receipt_id}")
     public ResponseEntity<?> deleteReceipt(@PathVariable Long receipt_id) {
         return receiptRepository.findById(receipt_id)
