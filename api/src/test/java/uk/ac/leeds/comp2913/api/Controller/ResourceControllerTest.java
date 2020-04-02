b88e8abf762912b89ca0822234ffa89108acf4fa
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-02 03:00:38 +0100

    Example of mocking service layer in HTTP Controller tests

--- api/src/test/java/uk/ac/leeds/comp2913/api/Controller/ResourceControllerTest.java
+++ api/src/test/java/uk/ac/leeds/comp2913/api/Controller/ResourceControllerTest.java
@@ -1,20 +1,39 @@
 package uk.ac.leeds.comp2913.api.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.ac.leeds.comp2913.api.Domain.Model.Activity;
-import uk.ac.leeds.comp2913.api.Domain.Model.Resource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import uk.ac.leeds.comp2913.api.Domain.Model.Resource;
+import uk.ac.leeds.comp2913.api.Domain.Service.ResourceService;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -22,11 +41,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class ResourceControllerTest {
 
+    public static final String TENNIS_COURT = "Tennis Court";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private ResourceService resourceServiceMock;
 
     @BeforeEach
     void setUp() {
@@ -37,22 +61,61 @@ class ResourceControllerTest {
     }
 
     @Test
+    void createResourceWithCorrectAuthorities() throws Exception {
+        final Resource resource = new Resource();
+        resource.setName(TENNIS_COURT);
+        Resource result = new Resource();
+        when(resourceServiceMock.create(any()))
+                .thenReturn(resource);
+
+
+        mockMvc.perform(post("/resources")
+                .with(jwt(jwt -> jwt.subject("ch4mpy").claim("scope", "resource:create")))
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsBytes(resource)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(TENNIS_COURT)));
+    }
+
+    @Test
+    @Disabled
+    void createResourceWithoutCorrectAuthorities() throws Exception {
+        final Resource resource = new Resource();
+        resource.setName(TENNIS_COURT);
+        Resource result = new Resource();
+        when(resourceServiceMock.create(any()))
+                .thenReturn(resource);
+
+        mockMvc.perform(post("/resources")
+                .with(jwt(jwt -> jwt.subject("ch4mpy")))
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsBytes(resource)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username = "test@comp2913.com")
     void getResources() throws Exception {
+
+        Resource resource = new Resource();
+        resource.setName(TENNIS_COURT);
+
+
+        Pageable request = PageRequest.of(0, 10);
+        Page<Resource> response = new PageImpl<>(List.of(resource), request, 1);
+
+
+        when(resourceServiceMock.findAll(any()))
+                .thenReturn(response);
+
         mockMvc.perform(get("/resources")
                 .contentType("application/json"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[*].name", Matchers.contains(TENNIS_COURT)));
     }
 
     @Test
-    void createResource() throws Exception {
-        mockMvc.perform(post("/resources/")
-                .contentType("application/json")
-                //.param("", "")
-                .content(objectMapper.writeValueAsBytes(new Resource())))
-                .andExpect(status().isOk());
-    }
-
-    @Test
+    @WithMockUser(username = "test@comp2913.com")
     void updateResource() throws Exception {
         mockMvc.perform(put("/resources/{resource_id}", 1)
                 .contentType("application/json")
@@ -62,10 +125,10 @@ class ResourceControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "test@comp2913.com")
     void deleteResource() throws Exception {
         mockMvc.perform(delete("/resources/{resource_id}", 1)
                 .contentType("application/json"))
-                //.param("", "")
                 .andExpect(status().isOk());
     }
 }
