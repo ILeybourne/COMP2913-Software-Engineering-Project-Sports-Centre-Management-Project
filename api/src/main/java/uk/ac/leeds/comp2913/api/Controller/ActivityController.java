0e28a158dd875210a7dcf0938bb7d873d63c58a8
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-02 02:53:37 +0100

    Cleaning ActivityController.java, adding comments for guidance Adding Mock user annotation to the Tests and changing Endpoint to match

--- api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ActivityController.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ActivityController.java
@@ -6,27 +6,37 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import uk.ac.leeds.comp2913.api.DataAccessLayer.Repository.ActivityRepository;
 import uk.ac.leeds.comp2913.api.DataAccessLayer.Repository.ActivityTypeRepository;
+import uk.ac.leeds.comp2913.api.DataAccessLayer.Repository.ResourceRepository;
 import uk.ac.leeds.comp2913.api.Domain.Model.Activity;
+import uk.ac.leeds.comp2913.api.Domain.Model.ActivityType;
+import uk.ac.leeds.comp2913.api.Domain.Model.Resource;
 import uk.ac.leeds.comp2913.api.Exception.ResourceNotFoundException;
 
 import javax.validation.Valid;
+
 import java.util.List;
 
+/**
+ * TODO: @CHORE, annotate with Swagger API documentation
+ * TODO: @CHORE, move domain logic into a service @DEPENDENCY for Testing
+ * TODO: @CHORE, add HAL to all endpoints
+ * TODO: @CHORE, add hasAuthority checks to all endpoints
+ */
 @RestController
+@RequestMapping("/activities")
 public class ActivityController {
 
     private final ActivityRepository activityRepository;
-
     private final ActivityTypeRepository activityTypeRepository;
 
     @Autowired
-    public ActivityController(ActivityRepository activityRepository, ActivityTypeRepository activityTypeRepository) {
-      this.activityRepository = activityRepository;
-      this.activityTypeRepository = activityTypeRepository;
+    public ActivityController(ActivityRepository activityRepository, ActivityTypeRepository activityTypeRepository, ResourceRepository resourceRepository) {
+        this.activityRepository = activityRepository;
+        this.activityTypeRepository = activityTypeRepository;
     }
-
 
     /**
      * Get all scheduled activities in the database
@@ -34,61 +44,59 @@ public class ActivityController {
      * @param pageable Pagination Metadata
      * @return a page of Activities
      */
-    @GetMapping("/activities")
+    @GetMapping("")
     public Page<Activity> getActivities(Pageable pageable) {
-      return activityRepository.findAll(pageable);
+        return activityRepository.findAll(pageable);
     }
 
-    //get scheduled activities by resource ID
-    @GetMapping("/resources/{resource_id}/activities")
+    //get scheduled activities for a given resource
+    @GetMapping("/resource/{resource_id}")
     public List<Activity> getActivitiesByResourceId(@PathVariable Long resource_id) {
         return activityRepository.findByResourceId(resource_id);
     }
 
     //schedule an activity
-    //Pulls data from activity type, only start and end type is pulled from json
-    //need to look at deducting current capacity when bookings are made...
-    @PostMapping("/activities/{activity_type_id}")
-    public Activity createActivity(@PathVariable Long activity_type_id, @Valid @RequestBody Activity activity) {
-      return activityTypeRepository.findById(activity_type_id)
-                .map(activityType -> {
-                  activity.setCost(activityType.getCost());
-                  activity.setCurrentCapacity(activityType.getTotalCapacity());
-                  activity.setName(activityType.getName());
-                  activity.setResource(activityType.getResource());
-                  activity.setStartTime(activity.getStartTime());
-                  activity.setEndTime(activity.getEndTime());
-                  return activityRepository.save(activity);
-                }).orElseThrow(() -> new ResourceNotFoundException("Activity Type not found with id " + activity_type_id));
+    //Pulls data from activity type, only start and end type is pulled from json via JsonCreator
+    @PostMapping("activitytype/{activity_type_id}")
+    public Activity createActivity(@PathVariable Long resource_id, @PathVariable Long activity_type_id, @Valid @RequestBody Activity activity) {
+        ActivityType activityType = activityTypeRepository.findById(activity_type_id)
+                .orElseThrow(() -> new ResourceNotFoundException("The Activity Type " + resource_id + " could not be found"));
+
+        activity.setCost(activityType.getCost());
+        activity.setCurrentCapacity(activityType.getTotalCapacity());
+        activity.setName(activityType.getName());
+        activity.setResource(activityType.getResource());
+
+        return activityRepository.save(activity);
     }
 
     //update details of scheduled activity
-    @PutMapping("/activities/{activity_id}")
+    @PutMapping("/{activity_id}")
     public Activity updateActivity(@PathVariable Long activity_id, @Valid @RequestBody Activity activityRequest) {
 
-        return activityRepository.findById(activity_id)
-                .map(activity -> {
-                    activity.setName(activityRequest.getName());
-                    activity.setCost(activityRequest.getCost());
-                    activity.setStartTime(activityRequest.getStartTime());
-                    activity.setEndTime(activityRequest.getEndTime());
-                    activity.setCurrentCapacity(activityRequest.getCurrentCapacity());
-//                    TODO: Others
-                  return activityRepository.save(activity);
-                }).orElseThrow(() -> new ResourceNotFoundException("Activity not found with ID " + activity_id));
+        Activity activity = activityRepository.findById(activity_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Activity not found with ID " + activity_id));
+
+        activity.setName(activityRequest.getName());
+        activity.setCost(activityRequest.getCost());
+        activity.setStartTime(activityRequest.getStartTime());
+        activity.setEndTime(activityRequest.getEndTime());
+        activity.setCurrentCapacity(activityRequest.getCurrentCapacity());
+
+        return activityRepository.save(activity);
     }
 
     //delete scheduled activity
-    @DeleteMapping("/activities/{activity_id}")
+    @DeleteMapping("/{activity_id}")
     public ResponseEntity<?> deleteActivity(@PathVariable Long activity_id) {
 
-      try {
-        activityRepository.deleteById(activity_id);
-        return ResponseEntity
-          .noContent()
-          .build();
-      } catch (EmptyResultDataAccessException e){
-        throw new ResourceNotFoundException("Activity not found with that ID " + activity_id);
-      }
+        try {
+            activityRepository.deleteById(activity_id);
+            return ResponseEntity
+                    .noContent()
+                    .build();
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Activity not found with that ID " + activity_id);
+        }
     }
 }
--- api/src/test/java/uk/ac/leeds/comp2913/api/Controller/ActivityControllerTest.java
+++ api/src/test/java/uk/ac/leeds/comp2913/api/Controller/ActivityControllerTest.java
@@ -1,6 +1,7 @@
 package uk.ac.leeds.comp2913.api.Controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
 import uk.ac.leeds.comp2913.api.Domain.Model.Activity;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -35,15 +41,17 @@ class ActivityControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "test@comp2913.com")
     void getActivitiesByResourceId() throws Exception {
-        mockMvc.perform(get("/resources/{resource_id}/activities", 1)
+        mockMvc.perform(get("activities/resource/{resource_id}", 1)
                 .contentType("application/json"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser(username = "test@comp2913.com")
     void addActivity() throws Exception {
-        mockMvc.perform(post("/resources/{resource_id}/activities", 1)
+        mockMvc.perform(post("/activities/activitytype/{activity_type_id}", 1)
                 .contentType("application/json")
                 //.param("", "")
                 .content(objectMapper.writeValueAsBytes(new Activity())))
@@ -51,19 +59,20 @@ class ActivityControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "test@comp2913.com")
     void updateActivity() throws Exception {
-        mockMvc.perform(put("/resources/{resource_id}/activities/{activity_id}", 1, 1)
+        mockMvc.perform(put("activities/{activity_id}", 1)
                 .contentType("application/json")
-                //.param("", "")
                 .content(objectMapper.writeValueAsBytes(new Activity())))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser(username = "test@comp2913.com")
     void deleteActivity() throws Exception {
-        mockMvc.perform(delete("/resources/{resource_id}/activities/{activity_id}", 1, 1)
+        /*TODO set up mock*/
+        mockMvc.perform(delete("activities/{activity_id}", 1)
                 .contentType("application/json"))
-                //.param("", "")
                 .andExpect(status().isOk());
     }
 }
