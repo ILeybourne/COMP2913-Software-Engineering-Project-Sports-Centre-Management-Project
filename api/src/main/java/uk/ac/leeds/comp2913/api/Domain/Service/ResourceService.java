14dac63b8b689aa105e84841ee660dc89dcf9743
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-02 03:05:38 +0100

    Enabling method security and implementing resource service

---
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Domain/Service/ResourceService.java
@@ -0,0 +1,27 @@
+package uk.ac.leeds.comp2913.api.Domain.Service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+import uk.ac.leeds.comp2913.api.Domain.Model.Resource;
+import uk.ac.leeds.comp2913.api.Exception.ResourceNotFoundException;
+
+@Service
+public interface ResourceService {
+    @Transactional
+    Resource create(Resource resource);
+
+    @Transactional
+    Resource findById(Long resource_id);
+
+    @Transactional
+    Resource update(Long resource_id, Resource resourceRequest);
+
+    @Transactional
+    void deleteById(Long resource_id) throws ResourceNotFoundException;
+
+    Page<Resource> findAll(Pageable pageable);
+}
