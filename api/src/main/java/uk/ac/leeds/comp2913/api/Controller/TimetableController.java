d8d77647bf3623ff06271af40a0ebcc701b5e889
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-02 02:59:27 +0100

    Cleaning TimetableController

--- api/src/main/java/uk/ac/leeds/comp2913/api/Controller/TimetableController.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Controller/TimetableController.java
@@ -9,23 +9,27 @@ import uk.ac.leeds.comp2913.api.Domain.Model.Activity;
 
 import java.util.Collection;
 
+/**
+ * TODO: @CHORE, annotate with Swagger API documentation
+ * TODO: @CHORE, move domain logic into a service
+ * TODO: @CHORE, add HAL to all endpoints, with links to where the client can find
+ * *          the associated resource, account and activity  for the booking
+ * TODO: @CHORE, add hasAuthority checks to all endpoints
+ */
 @RestController
 @RequestMapping("/timetable")
 public class TimetableController {
 
-  private final ActivityRepository activityRepository;
+    private final ActivityRepository activityRepository;
 
-  @Autowired
-  public TimetableController(ActivityRepository activityRepository) {
-    this.activityRepository = activityRepository;
-  }
+    @Autowired
+    public TimetableController(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
 
-  @GetMapping("")
-  public Collection<Activity> getResources() {
-    return activityRepository.findAllWithResources();
-//      .stream()
-//      .map(ActivityToTimetableDTOAdapter::map)
-//      .collect(Collectors.toList());
-  }
+    @GetMapping("")
+    public Collection<Activity> getResources() {
+        return activityRepository.findAllWithResources();
+    }
 }
 
