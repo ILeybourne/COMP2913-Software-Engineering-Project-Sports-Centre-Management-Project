47c80d410b1de8016a2b6c707c1ea2381922208d
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-07 21:56:35 +0100

    WIP: rearranging and formatting grids

--- web-frontend/customer-app/src/components/Footer.vue
+++ web-frontend/customer-app/src/components/Footer.vue
@@ -1,48 +1,52 @@
 <template>
-  <footer class="container-fluid">
+  <footer class="container">
     <div class="row">
-      <div class="col-md-3 text-left">
+      <div class="col-4 text-left">
+        <h6 class="text-uppercase">About Us</h6>
+        <div class="row">
+          <div class="col">
+            <a class="text-muted">Brief about us paragraph.</a><br />
+            <a class="text-muted">Learn more </a
+            ><a href="/about" title="About">here</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <h6 class="text-uppercase">Contact</h6>
+        <div class="row">
+          <div class="col-3 text-left">
+            <p><a>Email</a></p>
+            <p><a>Email</a></p>
+            <p><a>Phone</a></p>
+          </div>
+          <div class="col-9 text-left">
+            <p><a class="text-muted">email</a></p>
+            <p><a class="text-muted">email</a></p>
+            <p><a class="text-muted">phone number</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-2 text-left">
+        <h6 class="text-uppercase">Useful Links</h6>
+        <div class="row">
+          <div class="col">
+            <p><a href="/timetable" title="Timetable">Timetable</a></p>
+            <p><a href="/facilities" title="Facilities">Facilities</a></p>
+            <p><a href="/profile" title="Profile">Profile</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-2 text-left">
         <div class="line">
           <a href="/home" title="Home">
-            <img src="../assets/logo.png" style="height: 48px; width: 48px" />
+            <div class="text-center">
+              <img src="../assets/logo.png" style="height: 64px; width: 64px" />
+            </div>
           </a>
         </div>
-        <div class="line">
-          <em>
-            "We're better than you, and we know it!"
-          </em>
+        <div class="text-center text-muted">
+          <em>Awesome slogan</em>
         </div>
-        <div class="line">
-          <a>
-            Dedicated to your sporting experience.
-          </a>
-        </div>
-        <div class="line">
-          <a>Learn more</a>
-          <a href="/about" title="About">
-            about us
-          </a>
-        </div>
-      </div>
-      <div class="col-md-6 text-center">
-        <h6>Find Us</h6>
-      </div>
-      <div class="col-md-3 text-right">
-        <h6>Contacts:</h6>
-        <ul class="list-unstyled text-small">
-          <li>
-            <a class="text-muted">street address</a>
-          </li>
-          <li>
-            <a class="text-muted">email address</a>
-          </li>
-          <li>
-            <a class="text-muted">website</a>
-          </li>
-          <li>
-            <a class="text-muted">phone number</a>
-          </li>
-        </ul>
       </div>
     </div>
   </footer>
