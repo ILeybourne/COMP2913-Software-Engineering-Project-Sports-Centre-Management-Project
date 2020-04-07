5f5a31686950c007a52a1155c88a0f89cd695efd
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-07 10:26:45 +0100

    WIP: added logo.png from assets, with link to home

--- web-frontend/customer-app/src/components/Footer.vue
+++ web-frontend/customer-app/src/components/Footer.vue
@@ -2,28 +2,24 @@
   <footer class="container-fluid">
     <div class="row">
       <div class="col-md-3 text-left">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="d-block mb-2"
-        >
-          <circle cx="24" cy="24" r="20"></circle>
-        </svg>
-        <a>
-          "We're better than you, and we know it!"
-        </a>
-        <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">About us</a></li>
-          </ul>
-        </nav>
+        <div class="line">
+          <a href="/home" title="Home">
+            <img
+              src="../assets/logo.png"
+              style="height: 48px; width: 48px"
+            />
+          </a>
+        </div>
+        <div class="line">
+          <a>
+            "We're better than you, and we know it!"
+          </a>
+        </div>
+        <div class="line">
+          <a href="/about" title="About">
+            About Us
+          </a>
+        </div>
       </div>
       <div class="col-md-6 text-center">
         <h6>Find Us</h6>
