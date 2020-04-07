fdf9639379c2d11bdf3021a882a760e91e0d62e1
sc18eda <sc18eda@leeds.ac.uk>
2020-04-07 12:43:28 +0100

    Booking table changed to show correct info and edit and delete buttons added

--- web-frontend/customer-app/src/components/BookingTable.vue
+++ web-frontend/customer-app/src/components/BookingTable.vue
@@ -1,35 +1,30 @@
 <template>
-  <div id="app">
-    <h3 class="title">Bookings Table</h3>
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="sessions"
-      :single-select="true"
-      item-key="name"
-      show-select
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-switch
-          v-model="singleSelect"
-          label="Single select"
-          class="pa-3"
-        ></v-switch>
-      </template>
-    </v-data-table>
-
-    <b-modal id="edit-booking-modal" title="Create Activity" hide-footer>
-      <div class="d-flex justify-content-between">
-        <b-button
-          type="reset"
-          variant="danger"
-          @click="$bvModal.hide('edit-booking-modal')"
-          >Delete
-        </b-button>
-      </div>
-    </b-modal>
-  </div>
+  <v-data-table
+          :headers="headers"
+          :items="sessions"
+  >
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-spacer></v-spacer>
+        <v-dialog  max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+          </template>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon small @click="deleteItem(item)">
+        mdi-delete
+      </v-icon>
+      <v-icon small class="mr-2" @click="printItem(item)">
+        midi-print
+      </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <style scoped></style>
@@ -71,18 +66,13 @@ export default {
           sortable: true
         },
         {
-          value: "account",
-          text: "Account",
+          value: "resource.name",
+          text: "Facility",
           sortable: true
         },
         {
-          value: "email",
-          text: "Email",
-          sortable: true
-        },
-        {
-          value: "receipt",
-          text: "Receipt",
+          value: "actions",
+          text: "Actions",
           sortable: false
         }
       ]
@@ -98,6 +88,16 @@ export default {
     }),
     showCancel() {
       this.$bvModal.show("edit-booking-modal");
+    },
+    editItem(item) {
+      this.editedIndex = this.sessions.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    deleteItem(item) {
+      const index = this.sessions.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.sessions.splice(index, 1);
     }
   },
 
