6986139baa46a55ff304916e9883e0971bf16b60
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-07 09:25:59 +0100

    WIP: some minor formatting

--- web-frontend/customer-app/src/components/Footer.vue
+++ web-frontend/customer-app/src/components/Footer.vue
@@ -1,19 +1,48 @@
 <template>
-  <div class="footer">
-    <div class="footer-container">
-      <div class="footer-box">
-        Logo <br />
-        Slogan <br />
-        Brief About Us
+  <footer class="container-fluid">
+    <div class="row">
+      <div class="col-md-3 text-left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="d-block mb-2"
+        >
+          <circle cx="24" cy="24" r="20"></circle>
+        </svg>
+        <a>
+          "We're better than you, and we know it!"
+        </a>
+        <a>Brief About Us</a>
       </div>
-      <div class="footer-box">
-        Find Us
+      <div class="col-md-6 text-center">
+        <h6>Find Us</h6>
       </div>
-      <div class="footer-box">
-        Contact Details
+      <div class="col-md-3 text-right">
+        <h6>Contacts:</h6>
+        <ul class="list-unstyled text-small">
+          <li>
+            <a class="text-muted">street address</a>
+          </li>
+          <li>
+            <a class="text-muted">email address</a>
+          </li>
+          <li>
+            <a class="text-muted">website</a>
+          </li>
+          <li>
+            <a class="text-muted">phone number</a>
+          </li>
+        </ul>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <style scoped>
