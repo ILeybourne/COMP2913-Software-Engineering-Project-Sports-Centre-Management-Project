33973bf2727bc36ab0807126bbcdabd1ee9ae256
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-15 00:16:15 +0100

    Fixing various frontned issues and adding global styling

--- web-frontend/customer-app/src/components/NavBar.vue
+++ web-frontend/customer-app/src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+  <header class="header-section">
+    <b-navbar toggleable="lg">
       <b-navbar-brand to="/">Home</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse class="main-menu" id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/about">About</b-nav-item>
           <b-nav-item to="/timetable">Timetable</b-nav-item>
@@ -16,25 +16,27 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown v-if="isAuthenticated" right>
-            <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
-              <em>{{ user.name }}</em>
-            </template>
-            <div>
-              <b-dropdown-item v-if="isAuthenticated" to="/profile"
-                >Profile
-              </b-dropdown-item>
-              <b-dropdown-item v-if="isAuthenticated" @click="logout">
-                Log out
-              </b-dropdown-item>
-            </div>
-          </b-nav-item-dropdown>
-          <b-nav-item v-else @click="login">Log in </b-nav-item>
+          <div class="hr-box">
+            <b-nav-item-dropdown v-if="isAuthenticated" right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>{{ user.name }}</em>
+              </template>
+              <div>
+                <b-dropdown-item v-if="isAuthenticated" to="/profile"
+                  >Profile
+                </b-dropdown-item>
+                <b-dropdown-item v-if="isAuthenticated" @click="logout">
+                  Log out
+                </b-dropdown-item>
+              </div>
+            </b-nav-item-dropdown>
+            <b-nav-item v-else @click="login">Log in </b-nav-item>
+          </div>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
-  </div>
+  </header>
 </template>
 
 <script>
