1ba072e2b616f783427daff06cc54a91c538f889
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-14 15:01:20 +0100

    WIP: picture alignment, bookingtable link

--- web-frontend/customer-app/src/views/Profile.vue
+++ web-frontend/customer-app/src/views/Profile.vue
@@ -1,42 +1,48 @@
 <template>
-  <div class="container-flex">
-    <h1>Your Profile</h1>
-    <div id="profile-container">
-      <div class="row">
-        <div id="left-column" class="col-sm-7">
-          <div id="profile-card" class="card">
-            <div id="profile-picture-container">
+  <div id="profile-container">
+    <h1>Profile</h1>
+    <div class="row">
+      <div id="left-column" class="col-sm-7">
+        <div id="profile-card" class="card">
+          <div class="row">
+            <div class="col-xl-5 text-center align-self-center">
               <img
                 :src="user.picture"
                 id="profile-picture"
-                class="rounded border-bottom border-dark"
+                class="rounded rounded-circle border-bottom border-dark"
               />
             </div>
-            <div>
+            <div class="col-xl-7">
               <h2>{{ $auth.user.nickname }}</h2>
               <ul class="list-unstyled">
                 <li>{{ $auth.user.name }}</li>
                 <li>{{ $auth.user.email }}</li>
               </ul>
+              <ul class="list-unstyled">
+                <li>Other stuff</li>
+                <li>
+                  <a href="/bookingtable" title="BookingsTable">My Bookings</a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div id="right-column" class="col-sm-5 align-self-center">
-          <div id="membership-card" class="card">
-            <h2>Membership</h2>
-            <a>Membership details</a>
-          </div>
-          <div class="text-center">
-            <button
-              id="cancel-membership-btn"
-              type="button"
-              class="btn btn-outline-primary  "
-            >
-              Cancel Membership
-            </button>
-          </div>
-          <!--<pre>{{ JSON.stringify(user, null, 2) }}</pre>-->
+      </div>
+      <div id="right-column" class="col-sm-5 align-self-center">
+        <div id="membership-card" class="card">
+          <h2>Membership</h2>
+          <a>Membership details</a>
         </div>
+        <div class="text-center">
+          <button
+            id="cancel-membership-btn"
+            type="button"
+            class="btn btn-outline-primary  "
+          >
+            Cancel Membership
+          </button>
+        </div>
+        <!--<pre>{{ JSON.stringify(user, null, 2) }}</pre>-->
       </div>
     </div>
   </div>
@@ -55,18 +61,13 @@ export default {
 
 <style scoped>
 #profile-container {
-  margin-top: 5%;
-  margin-left: 10%;
-  margin-right: 10%;
+  margin: 5% 10%;
 }
 #left-column {
   min-width: 300px;
 }
 #profile-card {
-  padding: 15px;
-}
-#profile-picture-container {
-  padding-bottom: 24px;
+  padding: 24px;
 }
 #profile-picture {
   width: 200px;
@@ -75,9 +76,9 @@ export default {
   padding-top: 24px;
 }
 #membership-card {
-  padding: 15px;
+  padding: 24px;
 }
 #cancel-membership-btn {
-  margin-top: 15px;
+  margin-top: 24px;
 }
 </style>
