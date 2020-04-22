3d930f4d86e424716b0b363fa621e80d2c42dc27
Seb Garwood <sebastiangarwood@outlook.com>
2020-04-22 23:04:29 +0100

    Renaming Assembler

--- api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ActivityController.java
+++ api/src/main/java/uk/ac/leeds/comp2913/api/Controller/ActivityController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +16,13 @@ import uk.ac.leeds.comp2913.api.Domain.Model.Activity;
 import uk.ac.leeds.comp2913.api.Domain.Model.RegularSession;
 import uk.ac.leeds.comp2913.api.Domain.Service.ActivityService;
 import uk.ac.leeds.comp2913.api.ViewModel.ActivityDTO;
-import uk.ac.leeds.comp2913.api.ViewModel.Assembler.ActivityPagedResourcesAssembler;
+import uk.ac.leeds.comp2913.api.ViewModel.Assembler.ActivityDTOAssembler;
 
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * TODO: @CHORE, annotate with Swagger API documentation
@@ -44,14 +40,14 @@ public class ActivityController {
 
     private final ActivityService activityService;
     private final PagedResourcesAssembler<Activity> pagedResourcesAssembler;
-    private final ActivityPagedResourcesAssembler activityPagedResourcesAssembler;
+    private final ActivityDTOAssembler activityDTOAssembler;
 
 
     @Autowired
-    public ActivityController(ActivityService activityService, PagedResourcesAssembler<Activity> pagedResourcesAssembler, ActivityPagedResourcesAssembler activityPagedResourcesAssembler) {
+    public ActivityController(ActivityService activityService, PagedResourcesAssembler<Activity> pagedResourcesAssembler, ActivityDTOAssembler activityDTOAssembler) {
         this.activityService = activityService;
         this.pagedResourcesAssembler = pagedResourcesAssembler;
-        this.activityPagedResourcesAssembler = activityPagedResourcesAssembler;
+        this.activityDTOAssembler = activityDTOAssembler;
     }
 
     /**
@@ -64,7 +60,7 @@ public class ActivityController {
             description = "Get List of all scheduled activities and links to view the activitys details")
     public PagedModel<ActivityDTO> getActivities(Pageable pageable) {
         Page<Activity> allActivities = activityService.getActivities(pageable);
-        return pagedResourcesAssembler.toModel(allActivities, activityPagedResourcesAssembler);
+        return pagedResourcesAssembler.toModel(allActivities, activityDTOAssembler);
     }
 
     //get single activity
@@ -72,7 +68,7 @@ public class ActivityController {
     @Operation(summary = "Get specific scheduled activity",
             description = "Get a specific scheduled activity, and links to relevant operations")
     public ActivityDTO getActivityByActivityId(@Parameter(description = "The ID of the specific activity", required = true)@PathVariable Long activity_id) {
-        ActivityDTO activity = activityPagedResourcesAssembler.toModel(activityService.findActivityById(activity_id));
+        ActivityDTO activity = activityDTOAssembler.toModel(activityService.findActivityById(activity_id));
         activity.add(linkTo(ActivityController.class).slash(activity_id).withRel("delete"));
         activity.add(linkTo(ActivityController.class).slash(activity_id).withRel("update"));
         activity.add(linkTo(BookingController.class).slash("activity").slash(activity_id).withRel("Bookings"));
@@ -88,29 +84,31 @@ public class ActivityController {
     @Operation(summary = "Get scheduled activities for a given resource",
             description = "Get list of all scheduled activities for a particular facility#2")
     public PagedModel<ActivityDTO> getActivitiesByResourceId(Pageable pageable, @Parameter(description = "The ID of the specific resource(facility)", required = true)@PathVariable Long resource_id) {
-        return pagedResourcesAssembler.toModel(activityService.findByResourceId(pageable, resource_id), activityPagedResourcesAssembler);
+        return pagedResourcesAssembler.toModel(activityService.findByResourceId(pageable, resource_id), activityDTOAssembler);
     }
 
     //schedule an activity
     //Pulls data from activity type, only start and end type is pulled from json via JsonCreator
     //schedule an activity. Create a one time activity or regular session
-    @PostMapping("activitytype/{activity_type_id}")
+    @PostMapping("/activitytype/{activity_type_id}")
     @Transactional
     @Operation(summary = "create a new scheduled activity",
             description = "create a new scheduled activity, using activity type. Has the option to make regular session #12")
-    public Activity createActivity(@Parameter(description = "An ActivityDTO object, providing details needed to create an activity", required = true) @Valid @RequestBody ActivityDTO activityDTO,
+    public ActivityDTO createActivity(@Parameter(description = "An ActivityDTO object, providing details needed to create an activity", required = true) @Valid @RequestBody ActivityDTO activityDTO,
                                    @Parameter(description = "An ActivityDTO object, providing details needed to create an activity", required = true) @PathVariable Long activity_type_id) {
         Activity activity = new Activity();
         RegularSession regularSession = new RegularSession();
         activity.setStartTime(activityDTO.getStartTime());
         activity.setEndTime(activityDTO.getEndTime());
         activity.setSocial(activityDTO.isSocial());
+        activity.setResource(activityDTO.getResource());
         if (activityDTO.isRegularSession()) {
             regularSession.setInterval(activityDTO.getInterval());
         } else {
             regularSession = null;
         }
-        return activityService.createNewActivity(activity, activity_type_id, regularSession);
+        final Activity activity1 = activityService.createNewActivity(activity, activity_type_id, regularSession);
+        return activityDTOAssembler.toModel(activity1);
     }
 
     //update details of scheduled activity
