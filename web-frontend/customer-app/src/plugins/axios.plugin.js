42263e63bb1d866a61d0db715ac219d45599c832
Sven Buckland <pm17s2b@leeds.ac.uk>
2020-04-28 15:27:11 +0100

    WIP: Intercept 401 and 403, redirect 401 to login

--- web-frontend/customer-app/src/plugins/axios.plugin.js
+++ web-frontend/customer-app/src/plugins/axios.plugin.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getInstance } from "@/plugins/auth.plugin";
+import router from "@/router";
 //TODO may not be instance we want
 import store from "@/store";
 
@@ -29,6 +30,12 @@ authHttp.interceptors.response.use(
   error => {
     if (error.response.status === 400) {
       store.dispatch("validation/setValidationErrors", error.response.data);
+    }
+    if (error.response.status === 401) {
+      store.dispatch("auth/loginWithRedirect");
+    }
+    if (error.response.status === 403) {
+      router.push({ name: "Unauthorised" });
     }
     return Promise.reject(error);
   }
