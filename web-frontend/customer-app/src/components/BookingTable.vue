d7a2396348c7f91ea1fc413df51b866ac9eead39
sc18eda <sc18eda@leeds.ac.uk>
2020-05-06 13:42:56 +0100

    Commit for merge

--- web-frontend/customer-app/src/components/BookingTable.vue
+++ web-frontend/customer-app/src/components/BookingTable.vue
@@ -46,9 +46,6 @@
       </b-modal>
     </template>
     <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)">
-        mdi-pencil
-      </v-icon>
       <v-icon small @click="showDelete(item)">
         mdi-delete
       </v-icon>
